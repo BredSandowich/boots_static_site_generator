270d7caf49fd0d6292e80f39d2f99c795d509f30
BredSandowich <192383906+BredSandowich@users.noreply.github.com>
2026-03-06 09:53:44 -0700

    feature: implement markdown to html node conversion
    - Add markdown_to_html_node to convert documents to HTMLNode trees
    - Added helper functions for headings, lists, code, and quote blocks
    - Added helper function text_to_children to convert into a list of HTMLNodes
    - Fix inline italic parsing to support underscore delimiters as well

--- src/markdown_blocks.py
+++ src/markdown_blocks.py
@@ -1,4 +1,7 @@
 from enum import Enum
+from htmlnode import HTMLNode, ParentNode
+from inline_markdown import text_to_textnodes, text_node_to_html_node
+from textnode import TextNode
 
 class BlockType(Enum):
     PARAGRAPH = "paragraph"
@@ -51,3 +54,97 @@ def block_to_block_type(block):
         
     
     return BlockType.PARAGRAPH
+
+#Helper functions to get HTML Tags from Markdown Block
+def block_to_heading(block):
+    split = block.split(" ", 1)
+    header_num = split[0].count("#")
+    
+    content = split[1].strip()
+    
+    children = text_to_children(content)
+    return ParentNode(f"h{header_num}", children)
+        
+    
+def block_to_quote(block):
+    lines = block.split("\n")
+    quote = []
+    for line in lines:
+        if not line.startswith(">"):
+            raise ValueError("Invalid quote block")
+        quote.append(line.lstrip(">").strip)
+    content = " ".join(quote)
+    
+    children = text_to_children(content)
+    return ParentNode("blockquote", children)
+
+def block_to_uolist(block):
+    lines = block.split("\n")
+    list_items = []
+    for item in lines:
+        if not item.startswith("*") and not item.startswith("-"):
+            raise ValueError("Invalid list item")
+        line_content = item[2:].lstrip("*").lstrip("-").strip()
+        line_child_nodes = text_to_children(line_content)
+        list_items.append(ParentNode("li", line_child_nodes))
+    
+    return ParentNode("ul", list_items)
+
+def block_to_olist(block):
+    lines = block.split("\n")
+    list_items = []
+    for item in lines:
+        contents = item.split(". ", 1)
+        if len(contents) <2:
+            raise ValueError("Invalid ordered list item")
+        if not contents[0].isdigit():
+            raise ValueError("Invalid list item")
+        line_content = contents[1].strip()
+        line_child_nodes = text_to_children(line_content)
+        list_items.append(ParentNode("li", line_child_nodes))
+    
+    return ParentNode("ol", list_items)
+
+def block_to_code(block):
+    code_content = block[4:-3]
+    node = TextNode(code_content, "text")
+    html_node = text_node_to_html_node(node)
+    code_node = ParentNode("code", [html_node])
+    
+    return ParentNode("pre", [code_node])
+    
+#Helper function that takes a string and returns a list of HTMLNodes representing the inline markdown
+def text_to_children(text):
+    nodes = text_to_textnodes(text)
+    html_node_objects = []
+    for node in nodes:
+        html_node_objects.append(text_node_to_html_node(node))
+    
+    return html_node_objects
+
+
+#Convert full markdown document into a single parent HTMLNode
+def markdown_to_html_node(markdown):
+    split_blocks = markdown_to_blocks(markdown)
+    
+    children= []
+    for block in split_blocks:
+        block_type = block_to_block_type(block)
+        if block_type == BlockType.HEADING:
+            node = block_to_heading(block)
+        elif block_type == BlockType.CODE:
+            node = block_to_code(block)
+        elif block_type == BlockType.QUOTE:
+            node = block_to_quote(block)
+        elif block_type == BlockType.UNORDERED_LIST:
+            node = block_to_uolist(block)
+        elif block_type == BlockType.ORDERED_LIST:
+            node = block_to_olist(block)
+        else:
+            content = " ".join(block.split("\n"))
+            child = text_to_children(content)
+            node = ParentNode("p", child)
+        
+        children.append(node)
+    
+    return ParentNode("div", children)
